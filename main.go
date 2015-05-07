cdb6c521826a1576aacd45467b7b76628100fb65
Vic Vijayakumar <needcaffeine@gmail.com>
2015-05-06 21:33:45 -0400

    Allow program to listen on user-specified port
    yakdb now supports listening to a user-specified port.
    It will still run on :9532 if no port is specified.
    
    Resolves #1.

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
-	router := NewRouter()
-
+	// Read in the supplied flags from the command line to determine the port.
 	// By default, listen to port :9532 (:ykdb)
-	fmt.Println("Listening on port 9532...")
-	log.Fatal(http.ListenAndServe(":9532", router))
+	var portFlag int
+	flag.IntVar(&portFlag, "port", 9532, "Port to listen on for http requests.")
+	flag.Parse()
+	port := strconv.Itoa(portFlag)
+
+	router := NewRouter()
+	fmt.Printf("Listening on port %v...", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
